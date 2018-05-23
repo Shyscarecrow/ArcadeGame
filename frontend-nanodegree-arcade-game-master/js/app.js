3e441bacb60fc95f719aea040b8cff147c58c0e7
Alexandra <orlando2004@inbox.ru>
2018-05-23 18:05:57 +0300

    complete Enemy function

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/enemy-bug.png',
+	this.x = 0,
+	this.y = y,
+	this.speed = Math.random()*500;
+	
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +18,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	this.x += this.speed * dt;
+	if(this.x > 550) {
+		this.x = 0;
+	}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,18 +32,39 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+const Player = function(){
+	this.sprite = 'images/char-boy.png',
+	this.x = 0,
+	this.y = 405
+}
  
+ Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+		
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 let player = new Player();
+const coordinateY = [63, 146, 228];
 let allEnemies = [];
-for (i = 0; i <=10; i++){
-	let enemy = new Enemy();
-	allEnemies.push(enemy);
-}
+	for (i = 0; i<3; i++){
+		let enemy = new Enemy(coordinateY[i]);
+		allEnemies.push(enemy);
+	}
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
